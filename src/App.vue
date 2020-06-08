5b201227ed930a34fcb0f9f34133842d1f66854e
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-06-08 14:08:16 +0200

    add _medias.scss

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@
 			<router-link to="/">Home</router-link>
 			|
 			<router-link to="/about">About</router-link>
+			<h1 class="title">La course À l’espace</h1>
 		</div>
 		<router-view />
 	</div>
@@ -16,10 +17,23 @@
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
 	color: #2c3e50;
+	height: 100%;
+	& .title {
+		font-size: 144px;
+		text-transform: uppercase;
+		-webkit-text-stroke: 2px;
+		-webkit-text-stroke-color: $black;
+		color: transparent;
+		text-align: center;
+		margin-top: 15%;
+		@include smartphone {
+			font-size: 80px;
+		}
+	}
 }
 
 #nav {
-	padding: 30px;
+	// padding: 30px;
 
 	a {
 		font-weight: bold;
--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,8 @@ module.exports = {
 				prependData: `  
             @import "@/scss/vendors/_reset.scss";
             @import "@/scss/config/_global.scss";
-            @import "@/scss/config/_variables.scss";
+			@import "@/scss/config/_variables.scss";
+			@import "@/scss/config/_medias.scss";
           `,
 			},
 		},
