0eb734f74a95cf5446eaec9d0e035763304b373e
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-06-08 16:55:59 +0200

    creation of routes base & structure/style of 2 first pages

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,7 @@
 <template>
 	<div id="app">
 		<div id="nav">
-			<router-link to="/">Home</router-link>
-			|
-			<router-link to="/about">About</router-link>
-			<h1 class="title">La course À l’espace</h1>
+			<!-- <router-link to="/">Home</router-link> -->
 		</div>
 		<router-view />
 	</div>
@@ -16,13 +13,13 @@
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
 	text-align: center;
-	color: #2c3e50;
+	color: $white;
 	height: 100%;
 	& .title {
 		font-size: 144px;
 		text-transform: uppercase;
 		-webkit-text-stroke: 2px;
-		-webkit-text-stroke-color: $black;
+		-webkit-text-stroke-color: $white;
 		color: transparent;
 		text-align: center;
 		margin-top: 15%;
@@ -33,11 +30,9 @@
 }
 
 #nav {
-	// padding: 30px;
-
 	a {
 		font-weight: bold;
-		color: $black;
+		color: $white;
 
 		&.router-link-exact-active {
 			color: $white;
--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,8 @@ module.exports = {
 			sass: {
 				prependData: `  
             @import "@/scss/vendors/_reset.scss";
-            @import "@/scss/config/_global.scss";
 			@import "@/scss/config/_variables.scss";
+			@import "@/scss/config/_global.scss";
 			@import "@/scss/config/_medias.scss";
           `,
 			},
