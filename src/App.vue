fafe7b76decbfd3ffc7ca58c93fd8125770c1506
Kayse Sulejman <kaysse.sulejman@hotmail.fr>
2020-06-09 12:25:28 +0200

    added fonts

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,42 @@
 <template>
-	<div id="app">
-		<div id="nav">
-			<!-- <router-link to="/">Home</router-link> -->
-		</div>
-		<router-view />
-	</div>
+  <div id="app">
+    <div id="nav">
+      <!-- <router-link to="/">Home</router-link> -->
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
-	-webkit-font-smoothing: antialiased;
-	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: $white;
-	height: 100%;
-	& .title {
-		font-size: 144px;
-		text-transform: uppercase;
-		-webkit-text-stroke: 2px;
-		-webkit-text-stroke-color: $white;
-		color: transparent;
-		text-align: center;
-		margin-top: 15%;
-		@include smartphone {
-			font-size: 80px;
-		}
-	}
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: $white;
+  height: 100%;
+  & .title {
+    font-size: 144px;
+    text-transform: uppercase;
+    -webkit-text-stroke: 2px;
+    -webkit-text-stroke-color: $white;
+    color: transparent;
+    text-align: center;
+    margin-top: 15%;
+    @include smartphone {
+      font-size: 80px;
+    }
+  }
 }
 
 #nav {
-	a {
-		font-weight: bold;
-		color: $white;
+  a {
+    font-weight: bold;
+    color: $white;
 
-		&.router-link-exact-active {
-			color: $white;
-		}
-	}
+    &.router-link-exact-active {
+      color: $white;
+    }
+  }
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,26 @@
 <template>
-	<div class="home">
-		<h1 class="title">La course À l’espace</h1>
-		<NextButton />
-	</div>
+  <div class="home">
+    <h1 class="title">La course À l’espace</h1>
+    <NextButton />
+  </div>
 </template>
 
 <script>
-import NextButton from "@/components/NextButton.vue"
+import NextButton from "@/components/NextButton.vue";
 
 export default {
-	name: "Home",
-	components: {
-		NextButton,
-	},
-}
+  name: "Home",
+  components: {
+    NextButton,
+  },
+};
 </script>
 
 <style lang="scss">
 .title {
+  font-family: "Poppins";
+  font-weight: bold;
+  letter-spacing: 2px;
+  line-height: 125%;
 }
 </style>
