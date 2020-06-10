d195328d0ad5f81b0519bf95ba0124b4b0d30e75
T69M <thomas.monmousseau@hetic.net>
2020-06-10 17:02:28 +0200

    router

--- src/router/index.js
+++ src/router/index.js
@@ -6,45 +6,49 @@ import About from "../views/About.vue";
 Vue.use(VueRouter);
 
 const routes = [
-
-	{
-		path: "/",
-		name: "Home",
-		component: Home,
-		nextPage: "About",
-	},
-	{
-		path: "/about",
-		name: "About",
-		component: About,
-		nextPage: "Loading",
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-	},
-	{
-		path: "/loading",
-		name: "Loading",
-		nextPage: "Contexte",
-		component: () => import(/* webpackChunkName: "about" */ "@/views/Loading.vue"),
-	},
-	{
-		path: "/contexte",
-		name: "Contexte",
-		component: () => import("@/views/Contexte.vue"),
-	},
-	{
-		path: "/lancement",
-		name: "Lancement",
-		component: () => import("@/views/Lancement.vue"),
-	},
-	{
-		path: "/choice",
-		name: "Choice",
-		component: () => import("@/views/Choice.vue"),
-	},
-]
-
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    nextPage: "About",
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+    nextPage: "Loading",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+  },
+  {
+    path: "/loading",
+    name: "Loading",
+    nextPage: "Contexte",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "@/views/Loading.vue"),
+  },
+  {
+    path: "/contexte",
+    name: "Contexte",
+    component: () => import("@/views/Contexte.vue"),
+  },
+  {
+    path: "/lancement",
+    name: "Lancement",
+    component: () => import("@/views/Lancement.vue"),
+  },
+  {
+    path: "/choice",
+    name: "Choice",
+    component: () => import("@/views/Choice.vue"),
+  },
+  {
+    path: "/nasa",
+    name: "Nasa",
+    component: () => import("@/views/Nasa.vue"),
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
