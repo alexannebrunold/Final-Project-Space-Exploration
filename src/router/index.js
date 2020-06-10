37fab4423c0e0107b5a468b0134a79890253cf00
T69M <thomas.monmousseau@hetic.net>
2020-06-10 16:55:59 +0200

    Nasa Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,47 +1,53 @@
-import Vue from "vue"
-import VueRouter from "vue-router"
-import Home from "../views/Home.vue"
-import About from "../views/About.vue"
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-	{
-		path: "/",
-		name: "Home",
-		component: Home,
-		nextPage: "About",
-	},
-	{
-		path: "/about",
-		name: "About",
-		component: About,
-		nextPage: "Loading",
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-	},
-	{
-		path: "/loading",
-		name: "Loading",
-		nextPage: "Contexte",
-		component: () => import(/* webpackChunkName: "about" */ "../views/Loading.vue"),
-	},
-	{
-		path: "/contexte",
-		name: "Contexte",
-		component: () => import("../views/Contexte.vue"),
-	},
-	{
-		path: "/lancement",
-		name: "Lancement",
-		component: () => import("../views/Lancement.vue"),
-	},
-]
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    nextPage: "About",
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+    nextPage: "Loading",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+  },
+  {
+    path: "/loading",
+    name: "Loading",
+    nextPage: "Contexte",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Loading.vue"),
+  },
+  {
+    path: "/contexte",
+    name: "Contexte",
+    component: () => import("../views/Contexte.vue"),
+  },
+  {
+    path: "/lancement",
+    name: "Lancement",
+    component: () => import("../views/Lancement.vue"),
+  },
+  {
+    path: "/nasa",
+    name: "Nasa",
+    component: () => import("../views/Nasa.vue"),
+  },
+];
 
 const router = new VueRouter({
-	mode: "history",
-	routes,
-})
+  mode: "history",
+  routes,
+});
 
-export default router
+export default router;
