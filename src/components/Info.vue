21c9e08a4549eb69a246b5198a00fc974bd5fa02
Kayse Sulejman <kaysse.sulejman@hotmail.fr>
2020-06-11 12:30:06 +0200

    added title in info and started popup component

--- src/components/Info.vue
+++ src/components/Info.vue
@@ -1,105 +1,112 @@
 <template>
-	<div class="info" to="">
-		<div class="info__Img">
-			<img :src="imageUrl" />
-			<div class="info__Img__Quote">
-				{{ quoteInfo }}
-			</div>
-		</div>
-		<div class="info__Content">
-			<div class="info__Content__Subtitle">
-				{{ titleInfo }}
-			</div>
-			<div class="info__Content__Text">
-				{{ textInfo }}
-			</div>
-			<div class="info__Content__LearnMore">
-				<LearnMore />
-			</div>
-		</div>
-	</div>
+  <div class="info" to="">
+    <div class="info__Img">
+      <img :src="imageUrl" />
+      <div class="info__Img__Quote">
+        {{ quoteInfo }}
+      </div>
+    </div>
+
+    <div class="info__Content">
+      <div class="info__Content__Title">
+        {{ titleInfo }}
+      </div>
+      <div class="info__Content__Subtitle">
+        {{ subtitleInfo }}
+      </div>
+      <div class="info__Content__Text">
+        {{ textInfo }}
+      </div>
+      <div class="info__Content__LearnMore">
+        <LearnMore />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import LearnMore from "@/components/LearnMore.vue"
+import LearnMore from "@/components/LearnMore.vue";
 export default {
-	name: "Info",
-	components: {
-		LearnMore,
-	},
-	props: {
-		imageUrl: {},
-		quoteInfo: String,
-		titleInfo: String,
-		textInfo: String,
-	},
-}
+  name: "Info",
+  components: {
+    LearnMore,
+  },
+  props: {
+    imageUrl: {},
+    quoteInfo: String,
+    titleInfo: String,
+    subtitleInfo: String,
+    textInfo: String,
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .info {
-	font-family: "Poppins";
-	color: white;
-	display: flex;
-	background-color: $black;
-	&__Img {
-		width: 100%;
-		height: 100vh;
-		position: relative;
-		display: flex;
-		&__Quote {
-			background: black;
-			opacity: 40%;
-			left: 25%;
-			right: 25%;
-			padding: 10px;
-			font-size: 12pt;
-			position: absolute;
-			bottom: 25px;
-			& p {
-				background: none;
-			}
-		}
-	}
+  font-family: "Poppins";
+  color: white;
+  display: flex;
+  background-color: $black;
+  &__Img {
+    height: 100vh;
+    width: auto;
+    position: relative;
+    display: flex;
+    &__Quote {
+      background: black;
+      opacity: 40%;
+      left: 25%;
+      right: 25%;
+      padding: 10px;
+      font-size: 12pt;
+      position: absolute;
+      bottom: 25px;
+      & p {
+        background: none;
+      }
+    }
+  }
 
-	&__Content {
-		display: flex;
-		flex-direction: column;
-		padding-top: 50px;
-		text-align: left;
+  &__Content {
+    display: flex;
+    flex-direction: column;
+    padding-top: 80px;
+    text-align: left;
+    width: 50%;
+    margin: 0 auto;
+    &__Title {
+      width: fit-content;
+      margin: 0 auto;
+      font-size: 106px;
+      font-weight: bold;
+      text-transform: uppercase;
+      -webkit-text-stroke: 2px;
+      -webkit-text-stroke-color: $white;
+      color: transparent;
+      text-align: center;
+    }
 
-		&__Title {
-			width: fit-content;
-			margin: 0 auto;
-			font-size: 96px;
-			font-weight: bold;
-			text-transform: uppercase;
-			-webkit-text-stroke: 2px;
-			-webkit-text-stroke-color: $white;
-			color: transparent;
-			text-align: center;
-		}
+    &__Subtitle,
+    &__Text,
+    &__LearnMore,
+    &__NextButton {
+      padding-top: 55pt;
+      margin: 0 10%;
+      line-height: 120%;
+    }
 
-		&__Subtitle,
-		&__Text,
-		&__LearnMore,
-		&__NextButton {
-			padding-top: 55pt;
-			margin: 0 100px;
-			line-height: 120%;
-		}
+    &__Subtitle {
+      padding-top: 100px;
+      font-size: 58px;
+      font-weight: 300;
+      width: 85%;
+    }
 
-		&__Subtitle {
-			font-size: 48px;
-			font-weight: 300;
-			width: 85%;
-		}
-
-		&__Text {
-			font-size: 22px;
-			width: 85%;
-			font-weight: 200;
-		}
-	}
+    &__Text {
+      font-size: 32px;
+      width: 85%;
+      font-weight: 200;
+    }
+  }
 }
 </style>
