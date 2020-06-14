97cd7af155046ca927079671df55a27266deb647
T69M <thomas.monmousseau@hetic.net>
2020-06-14 17:19:06 +0200

    router

--- src/router/index.js
+++ src/router/index.js
@@ -2,88 +2,92 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/",
+    name: "Disclaimer",
+    component: () => import("@/views/Disclaimer.vue"),
+  },
+  {
+    path: "/home",
+    name: "Home",
+    component: Home,
+    nextPage: "contexte",
+  },
+  {
+    path: "/contexte",
+    name: "Contexte",
+    component: () => import("@/views/Contexte.vue"),
+    nextPage: "Lancement",
+  },
+  {
+    path: "/lancement",
+    name: "Lancement",
+    component: () => import("@/views/Lancement.vue"),
+  },
+  {
+    path: "/loading",
+    name: "Loading",
+    nextPage: "Contexte",
+    component: () => import("@/views/Loading.vue"),
+  },
+  {
+    path: "/liftoff",
+    name: "Liftoff",
+    component: () => import("@/views/Liftoff.vue"),
+  },
+  {
+    path: "/nasa",
+    name: "Nasa",
+    component: () => import("@/views/Nasa.vue"),
+    nextPage: "Choice",
+  },
+  {
+    path: "/choice",
+    name: "Choice",
+    component: () => import("@/views/Choice.vue"),
+  },
+  {
+    path: "/luna",
+    name: "Luna",
+    component: () => import("@/views/Luna.vue"),
+  },
+  {
+    path: "/apollo",
+    name: "Apollo",
+    component: () => import("@/views/Apollo.vue"),
+  },
 
-	{
-		path: "/",
-		name: "Disclaimer",
-		component: () => import("@/views/Disclaimer.vue"),
-	},
-	{
-		path: "/home",
-		name: "Home",
-		component: Home,
-		nextPage: "contexte",
-	},
-	{
-		path: "/contexte",
-		name: "Contexte",
-		component: () => import("@/views/Contexte.vue"),
-		nextPage: "Lancement",
-	},
-	{
-		path: "/lancement",
-		name: "Lancement",
-		component: () => import("@/views/Lancement.vue"),
-	},
-	{
-		path: "/loading",
-		name: "Loading",
-		nextPage: "Contexte",
-		component: () => import("@/views/Loading.vue"),
-	},
-	{
-		path: "/liftoff",
-		name: "Liftoff",
-		component: () => import("@/views/Liftoff.vue"),
-	},
-	{
-		path: "/nasa",
-		name: "Nasa",
-		component: () => import("@/views/Nasa.vue"),
-		nextPage: "Choice",
-	},
-	{
-		path: "/choice",
-		name: "Choice",
-		component: () => import("@/views/Choice.vue"),
-	},
-	{
-		path: "/luna",
-		name: "Luna",
-		component: () => import("@/views/Luna.vue"),
-	},
-	{
-		path: "/apollo",
-		name: "Apollo",
-		component: () => import("@/views/Apollo.vue"),
-	},
+  {
+    path: "/enSavoirPlus",
+    name: "EnSavoirPlus",
+    component: () => import("@/views/EnSavoirPlus.vue"),
+    nextPage: "Lifftoff",
+  },
 
-	{
-		path: "/enSavoirPlus",
-		name: "EnSavoirPlus",
-		component: () => import("@/views/EnSavoirPlus.vue"),
-		nextPage: "Lifftoff",
-	},
+  {
+    path: "/rocard",
+    name: "Rocard",
+    component: () => import("@/views/Rocard.vue"),
+  },
 
-	{
-		path: "/artemis",
-		name: "Artemis",
-		component: () => import("@/views/Artemis.vue"),
-	},
-	{
-		path: "/popUp/:name",
-		name: "PopUp",
-		component: () => import("@/views/PopUp.vue"),
-	},
-]
-
+  {
+    path: "/artemis",
+    name: "Artemis",
+    component: () => import("@/views/Artemis.vue"),
+  },
+  {
+    path: "/popUp/:name",
+    name: "PopUp",
+    component: () => import("@/views/PopUp.vue"),
+  },
+];
 
 const router = new VueRouter({
-	mode: "history",
-	routes,
-})
+  mode: "history",
+  routes,
+});
 
-export default router
+export default router;
