d423582e9bd7d6e37674ee3849b9ebd4c7bce10b
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-06-16 13:01:49 +0200

    + speed

--- src/views/Apollo.vue
+++ src/views/Apollo.vue
@@ -163,7 +163,7 @@ export default {
 		scrollHorizontal(e) {
 			e = window.event || e
 			let delta = Math.max(-1, Math.min(1, e.wheelDelta))
-			let scrollSpeed = 20
+			let scrollSpeed = 40
 			document.documentElement.scrollLeft -= delta * scrollSpeed
 			this.$refs.bodyScroll.scrollLeft -= delta * scrollSpeed
 			e.preventDefault()
