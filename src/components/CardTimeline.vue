78608df2b05bc9fba766fa1e34527d3678d72fa0
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-06-16 16:12:12 +0200

    style cards hover

--- src/components/CardTimeline.vue
+++ src/components/CardTimeline.vue
@@ -48,26 +48,33 @@ export default {
 	z-index: 2;
 	display: table;
 	height: 538px;
-	width: auto;
+
 	text-align: left;
 	padding-left: 1%;
 	padding-right: 1%;
-	& img {
-		&:hover {
-			transform: scale(1.1);
-			cursor: pointer;
-		}
+
+	&:hover {
+		transform: scale(1.16);
+		padding-top: 2px;
+		cursor: pointer;
+		margin-top: 8px;
+		background-color: #4141412d;
+		height: 400px;
+		border-radius: 8px;
+		width: 100px;
+		box-shadow: 20px 12px 8px rgb(22, 19, 19);
 	}
 	&__div {
 		display: table;
 		margin-bottom: 4%;
 		&--circle {
 			margin: 0 auto;
-			width: 16px;
-			height: 16px;
+			width: 20px;
+			height: 20px;
 			border-radius: 20px;
 			background: #c4c4c4;
 			margin-top: 4%;
+
 			&:hover {
 				background: $white;
 				cursor: pointer;
