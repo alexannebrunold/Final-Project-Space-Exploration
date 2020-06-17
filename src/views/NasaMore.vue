6677812554e4ec7dde1bd4f5fc9d69f53e46c7b6
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-06-17 18:44:53 +0200

    houbolt fixed

--- src/views/NasaMore.vue
+++ src/views/NasaMore.vue
@@ -3,7 +3,7 @@
 		<MoreInfo
 			class="enSavoirPlus__Info"
 			:imageUrlLeft="imageSrcLeft"
-			:imageUrlRight="urlVideo"
+			:videoUrlRight="urlVideo"
 			titleInfoLeft=""
 			titleInfoRight="NASA 60th: How It All Began"
 			textInfoLeft="Présentation des officiels de la NASA après la signature par le président Eisenhower du “National Aeronautics and Space Act” fondant la NASA le 29 juillet 1958. <br/>La NASA est fondée le 1 octobre 1958 pour administrer et réaliser les projets relevant de l'astronautique civile, jusque-là pris en charge par les différentes branches des forces armées des États-Unis, afin de rattraper l'avance prise par l'Union soviétique."
