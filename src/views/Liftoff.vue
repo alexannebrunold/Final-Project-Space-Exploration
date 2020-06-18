cfa6e509458161503aae4ebe7d956e880fd81d73
Alexanne Brunold <alexanne.brunold@hetic.net>
2020-06-19 01:24:07 +0200

    fix

--- src/views/Liftoff.vue
+++ src/views/Liftoff.vue
@@ -1,6 +1,12 @@
 <template>
-	<div class="liftoff page" @click="play()">
-		<video rel="preload" autoplay class="liftoff__video" @ended="$router.push('/nasa')">
+	<div class="liftoff page">
+		<video
+			rel="preload"
+			autoplay
+			class="liftoff__video"
+			@ended="$router.push('/nasa')"
+			@click="play()"
+		>
 			<source src="../assets/videos/liftoff.mp4" type="video/mp4" />
 		</video>
 		<div class="liftoff__button">
